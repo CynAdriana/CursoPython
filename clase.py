c6ac33264ddcff76acf63e9cabda3cf200c2d28b
Cyn Rod <016019315@upq.edu.mx>
2024-07-29 23:39:29 -0600

    Tipo de datos python

---
+++ clase.py
@@ -0,0 +1,39 @@
+#variables en python
+# name = "Cynthia"
+# lastNme = '''Adriana
+
+
+# Rodriguez'''
+# name1 = "Cynthia Adriana"
+# lastName = "Rod Martinez"
+# #imprimir tipo de variable
+# print(lastNme)
+# print(name1[0])
+# print(name1[-1])
+# #Concatenar
+# print(name1 +" "+ lastName)
+# #Replicacion
+# print(name1 * 5)
+# #Consultar la longitud 
+# print(len(lastNme))
+# #Metodos 
+# print(name.lower()) #minusculas
+# print(name.upper()) #mayusculas
+#int / float / boolean
+x = 10
+print(type(x))
+y= 5.425
+print(type(y))
+z= 1.2E6
+print(z)
+a = x + y
+print(a)
+is_true = True
+print(is_true)
+is_false = False
+print(is_false)
+
+
+
+
+
